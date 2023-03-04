93977019c1ac3b29652df65c5f588d9f9c9653ed
SamuelVanie <samuelmichaelvanie@gmail.com>
2023-03-04 23:42:49 +0100

    fix: remove deprecated lsp server of lua

--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua" }
+  ensure_installed = { "lua_ls" }
 })
 
 local on_attach = function(_, _)
@@ -13,7 +13,7 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
-require("lspconfig").sumneko_lua.setup {
+require("lspconfig").lua_ls.setup {
   on_attach = on_attach
 }
 
